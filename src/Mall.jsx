418becef95530d7d51e52979e5b6c5dd2bc6d488
NyentiPrecious <gildanyenti@gmail.com>
2022-06-11 12:07:33 +0100

    init

--- src/Mall.jsx
+++ src/Mall.jsx
@@ -3,8 +3,86 @@ import './App.css'
 
 function Mall() {
   return (
-    <div className="mall" >
-        Mallhhhhhhhhh
+  
+        <div class='Cart-Container'>
+
+<div  class='Header'>
+    <h1 class='Heading'>Shopping Cart</h1>
+    <h5 class='Action'>Remove all</h5>
+    </div>
+
+    <div class='Cart-Items'>
+
+        <div class='image-box'>
+hhhhhhhhhhhhhhhhhhhhhhh           
+        </div>
+
+        <div class='about'>
+        <h1 class='title'>Apple Juice</h1>
+        <h3 class='subtitle'>250ml</h3>
+       
+        </div>
+   
+
+
+            <div class='prices'>
+                <div class='amount'>$2.99</div>
+                <div class='save'><u>Save for later</u></div>
+                <div class='remove'><u>Remove</u></div>
+                </div>  
+
+            </div>
+
+
+
+            <div class='checkout'>
+
+
+            <div class='counter'>
+            
+            <div class='btn'>+</div>
+            <div class='count'>2</div>
+            <div class='btn'>-hhhhhhhhhh</div>
+            </div>  
+
+<button class='button'>Checkout</button>
+
+
+</div>   
+
+
+
+<div class='Cart-Items'>
+
+<div class='image-box'>
+   
+</div>
+
+<div class='about'>
+<h1 class='title'>Apple Juice</h1>
+<h3 class='subtitle'>250ml</h3>
+
+ 
+</div>
+
+
+
+<div class='counter'>
+    <div class='btn'>+</div>
+    <div class='count'>2</div>
+    <div class='btn'>-</div>
+    </div>  
+
+    <div class='prices'>
+        <div class='amount'>$2.99</div>
+        <div class='save'><u>Save for later</u></div>
+        <div class='remove'><u>Remove</u></div>
+        </div>  
+
+    </div>
+       
+
+
         </div>
   )
 }
